817be4ef956fa505f88bfde49d79eae0fa18cf30
Vito <vitocristiano@hotmail.com>
2021-06-07 00:20:05 +0200

    Commit update

--- pages/index.vue
+++ pages/index.vue
@@ -52,21 +52,11 @@
     <div class="container3">
       <h1 class="touch">Sitios</h1>
         <div id="animate2" class="roll">
-          <button id="myBtn">
-          <img id="reveal2" class="tour" src="~/assets/tour_2.png" alt="lobatera"></button>
+          <img id="reveal2" class="tour" src="~/assets/tour_2.png" alt="lobatera">
           <img id="reveal2" class="tour" src="~/assets/tour.png" alt="mukumbari">
           <img id="reveal2" class="tour" src="~/assets/tour_3.png" alt="observatorio">
+
         </div>
-        <!-- The Modal -->
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <p>Some text in the Modal..</p>
-  </div>
-
-</div>
     </div>
     <!--container4 rojo-->
     <div class="container4">
@@ -137,32 +127,6 @@ export default {
       // add indicators (requires plugin)
       //.setTween("#animate2", 0.5, {backgroundColor: "white"})
 
-  }
-}
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
   }
 }
 
@@ -453,41 +417,9 @@ p {
   padding-top: 15px;
 }
 
-/* The Modal (background) */
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-
-/* Modal Content/Box */
-.modal-content {
-  background-color: #fefefe;
-  margin: 15% auto; /* 15% from the top and centered */
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%; /* Could be more or less, depending on screen size */
-}
-
-/* The Close Button */
-.close {
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: black;
-  text-decoration: none;
-  cursor: pointer;
+@media screen and (max-width: 1100px) {
+  .container {
+    width: 80%;
+  }
 }
 </style>
